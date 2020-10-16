44bc72695c5e2ddcea29bfcfdd6ca72ba0945493
Priya Srikumar <priyasrikumar1@gmail.com>
2020-10-16 10:14:04 -0400

    finishing

--- a.c
+++ a.c
@@ -1,4 +1,9 @@
 int main() {
-	int x = 42;
-	return x;
+	gcd_iter(5, 6);
+}
+int gcd_iter(int u, int v) {
+  if (u < 0) u = -u;
+  if (v < 0) v = -v;
+  if (v) while ((u %= v) && (v %= u));
+  return (u + v);
 }
--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -2,29 +2,96 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/InstrTypes.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 using namespace llvm;
 
-namespace {
-  struct SkeletonPass : public FunctionPass {
+namespace
+{
+  struct SkeletonPass : public FunctionPass
+  {
     static char ID;
     SkeletonPass() : FunctionPass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-      errs() << "I saw a function called " << F.getName() << "!\n";
+    virtual bool runOnFunction(Function &F)
+    {
+      for (auto &B : F)
+      {
+        for (auto &I : B)
+        {
+          if (auto *op = dyn_cast<BinaryOperator>(&I))
+          {
+            // Insert at the point where the instruction `op` appears.
+            IRBuilder<> builder(op);
+
+            // Make a multiply with the same operands as `op`.
+            Value *lhs = op->getOperand(0);
+            Value *rhs = op->getOperand(1);
+            Value *res = nullptr;
+            switch (op->getOpcode())
+            {
+            case 13:
+              res = builder.CreateSub(lhs, rhs);
+              break;
+            case 14:
+              res = builder.CreateFSub(lhs, rhs);
+              break;
+            case 15:
+              res = builder.CreateAdd(lhs, rhs);
+              break;
+            case 16:
+              res = builder.CreateFAdd(lhs, rhs);
+              break;
+            case 17:
+              res = builder.CreateUDiv(lhs, rhs);
+              break;
+            case 18:
+              res = builder.CreateFDiv(lhs, rhs);
+              break;
+            case 19:
+              res = builder.CreateMul(lhs, rhs);
+              break;
+            case 20:
+              res = builder.CreateMul(lhs, rhs);
+              break;
+            case 21:
+              res = builder.CreateFMul(lhs, rhs);
+              break;
+            case 28:
+              res = builder.CreateOr(lhs, rhs);
+              break;
+            case 29:
+              res = builder.CreateAnd(lhs, rhs);
+              break;
+            default:
+              res = op;
+            }
+            errs() << "Old Instruction: " << op << "\n";
+            for (auto &U : op->uses())
+            {
+              User *user = U.getUser();
+              user->setOperand(U.getOperandNo(), res);
+            }
+            errs() << "Changed Instruction: " << op << "\n";
+            return true;
+          }
+        }
+      }
+
       return false;
     }
   };
-}
+} // namespace
 
 char SkeletonPass::ID = 0;
 
-// Automatically enable the pass.
-// http://adriansampson.net/blog/clangpass.html
 static void registerSkeletonPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
+                                 legacy::PassManagerBase &PM)
+{
   PM.add(new SkeletonPass());
 }
 static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerSkeletonPass);
+    RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
+                   registerSkeletonPass);
